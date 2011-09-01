e9764f15f44f67f14f254a441162ce9939f416a8
Andrei Neculau <andrei.neculau@klarna.com>
2011-09-01 11:08:30 +0200

    Refactored, added translate capital&uppercase

--- jquery.jsperanto.js
+++ jquery.jsperanto.js
@@ -1,143 +1,210 @@
-//jquery 1.3.2 dependencies  : $.each, $.extend, $.ajax
-
-(function($) {
+/*jslint browser:true */
+/*jshint browser:true */
+/*global
+  jQuery:true
+*/
+(function(window, document, $) {
 	//defaults
-	var o = {};
-    o.interpolationPrefix = '__';
-    o.interpolationSuffix = '__';
-    o.pluralSuffix = "_plural";
-    o.maxRecursion = 50; //used while applying reuse of strings to avoid infinite loop
-    o.reusePrefix = "$t(";
-    o.reuseSuffix = ")";
-    o.fallbackLang = 'en-US'; // see Language fallback section
-    o.dicoPath = 'locales'; // see Dictionary section
-    o.keyseparator = "."; // keys passed to $.jsperanto.translate use this separator
-    o.setDollarT = true; // $.t aliases $.jsperanto.translate, nice shortcut
-    o.dictionary = false; // to supply the dictionary instead of loading it using $.ajax. A (big) javascript object containing your namespaced translations
-	o.lang = false; //specify a language to use
-	o.pluralNotFound = ["plural_not_found_", Math.random()].join(''); // used internally by translate
-	
+	var o = {
+        'interpolationPrefix': '__',
+
+        'interpolationSuffix': '__',
+
+        'keyseparator': '.', // keys passed to $.jsperanto.translate use this separator
+
+        'pluralSuffix': '_plural',
+
+        'reusePrefix': '$t(',
+
+        'reuseSuffix': ')',
+
+        'maxRecursion': 50, //used while applying reuse of strings to avoid infinite loop
+
+        'setDollarT': true, // $.t aliases $.jsperanto.translate, nice shortcut
+
+        'dictionaryPath': 'locales', // see Dictionary section
+
+        'dictionary': false, // to supply the dictionary instead of loading it using $.ajax. A (big) javascript object containing your namespaced translations
+
+        'fallbackLang': 'en-US', // see Language fallback section
+
+        'lang': false //specify a language to use
+    };
+
+    var pluralNotFound = ['plural_not_found_', Math.random()].join(''); // used internally by translate
+	var countRecursion = 0;
 	var dictionary = false; //not yet loaded
 	var currentLang = false;
-	var count_of_replacement = 0;
-	
-	function init(callback,options){
-		$.extend(o,options);
-		if(!o.lang){o.lang = detectLanguage();}
-		loadDictionary(o.lang,function(loadedLang){
-			currentLang = loadedLang;
-			if(o.setDollarT){$.t = $.t || translate;} //shortcut
-			callback(translate);
-		});
-	}
-	
-	function applyReplacement(string,replacementHash){
-		$.each(replacementHash,function(key,value){
-			string = string.replace([o.interpolationPrefix,key,o.interpolationSuffix].join(''),value);
-		});
-		return string;
-	}
-	
-	function applyReuse(translated,options){
-		while (translated.indexOf(o.reusePrefix) != -1){
-			count_of_replacement++;
-			if(count_of_replacement > o.maxRecursion){break;} // safety net for too much recursion
-			var index_of_opening = translated.indexOf(o.reusePrefix);
-			var index_of_end_of_closing = translated.indexOf(o.reuseSuffix,index_of_opening) + o.reuseSuffix.length;
-			var token = translated.substring(index_of_opening,index_of_end_of_closing);
-			var token_sans_symbols = token.replace(o.reusePrefix,"").replace(o.reuseSuffix,"");
-			var translated_token = _translate(token_sans_symbols,options);
-			translated = translated.replace(token,translated_token);
-		}
-		return translated;
-	}
-	
-	function detectLanguage(){
-		if(navigator){
-			return (navigator.language) ? navigator.language : navigator.userLanguage;
-		}else{
+
+	var detectLanguage = function() {
+		if (navigator) {
+			return navigator.language ? navigator.language : navigator.userLanguage;
+		} else {
 			return o.fallbackLang;
 		}
-	}
-	
-	function needsPlural(options){
-		return (options.count && typeof options.count != 'string' && options.count > 1);
-	}
-	
+	};
 
-	function translate(dottedkey,options){
-		count_of_replacement = 0;
-		return _translate(dottedkey,options);
-	}
-	
-	/*
-	options.defaultValue
-	options.count
-	*/
-	function _translate(dottedkey,options){
-		options = options || {};
-		var notfound = options.defaultValue || dottedkey;
-		if(!dictionary){return notfound;} // No dictionary to translate from
-		
-		if(needsPlural(options)){
-			var optionsSansCount = $.extend({},options);
-			delete optionsSansCount.count;
-			optionsSansCount.defaultValue = o.pluralNotFound;
-			var pluralKey = dottedkey + o.pluralSuffix;
-			var translated = translate(pluralKey,optionsSansCount);
-			if(translated != o.pluralNotFound){
-				return applyReplacement(translated,{count:options.count});//apply replacement for count only
-			}// else continue translation with original/singular key
-		}
-		
-		var keys = dottedkey.split(o.keyseparator);
-		var i = 0;
-		var value = dictionary;
-        while(keys[i]) {
-            value = value && value[keys[i]];
-            i++;
-        }
-		if(value){
-			value = applyReplacement(value,options);
-			value = applyReuse(value,options);
-			return value;
-		}else{
-			return notfound;
-		}
-	}
-	
-	function loadDictionary(lang,doneCallback){
-		if(o.dictionary){
+	var loadDictionary = function(lang, doneCallback) {
+		if (o.dictionary) {
 			dictionary = o.dictionary;
 			doneCallback(lang);
 			return;
 		}
+
 		$.ajax({
-			url: [o.dicoPath,"/", lang, '.json'].join(''),
-			success: function(data,status,xhr){
+			'url': o.dictionaryPath + '/' + lang + '.json',
+
+			'success': function(data, status, xhr) {
 				dictionary = data;
 				doneCallback(lang);
 			},
-			error : function(xhr,status,error){
-				if(lang != o.fallbackLang){
-					loadDictionary(o.fallbackLang,doneCallback);
-				}else{
+
+			'error': function(xhr, status, error) {
+				if (lang != o.fallbackLang) {
+					loadDictionary(o.fallbackLang, doneCallback);
+				} else {
 					doneCallback(false);
 				}
 			},
-			dataType: "json"
+
+			'dataType': 'json'
 		});
-	}
-	
-	function lang(){
-		return currentLang;
-	}
-	
-	$.jsperanto = $.jsperanto || {
-		init:init,
-		t:translate,
-		translate:translate,
-		detectLanguage : detectLanguage,
-		lang : lang
 	};
-})(jQuery);
+
+	var applyReplacement = function(string, replacementHash) {
+		$.each(replacementHash, function(key, value) {
+			string = string.replace([o.interpolationPrefix, key, o.interpolationSuffix].join(''), value);
+		});
+		return string;
+	};
+
+	var applyReuse = function(translated, options) {
+		while (translated.indexOf(o.reusePrefix) != -1) {
+			countRecursion++;
+
+            // safety net for too much recursion
+			if (countRecursion > o.maxRecursion) {
+                break;
+            }
+
+			var indexOfOpening = translated.indexOf(o.reusePrefix);
+			var indexOfEndOfClosing = translated.indexOf(o.reuseSuffix, indexOfOpening) + o.reuseSuffix.length;
+			var token = translated.substring(indexOfOpening, indexOfEndOfClosing);
+			var tokenSansSymbols = token.replace(o.reusePrefix, '').replace(o.reuseSuffix, '');
+			var translatedToken = doTranslate(tokenSansSymbols, options);
+
+			translated = translated.replace(token, translatedToken);
+		}
+		return translated;
+	};
+
+	var needsPlural = function(options) {
+		return (options.count && typeof options.count !== 'string' && options.count > 1);
+	};
+
+
+	/*
+	options.defaultValue
+	options.count
+	*/
+	var doTranslate = function(dottedkey, options) {
+		options = $.extend({}, options);
+
+		var notFound = options.defaultValue ? options.defaultValue : dottedkey;
+
+        // No dictionary to translate from
+		if (!dictionary) {
+            return notFound;
+        }
+
+		if (needsPlural(options)) {
+			var optionsPlural = $.extend({}, options, {
+                'defaultValue': pluralNotFound,
+                'count': 1
+            });
+
+			var pluralKey = dottedkey + o.pluralSuffix;
+			var translated = translate(pluralKey, optionsPlural);
+
+            //apply replacement for count only
+			if (translated !== o.pluralNotFound) {
+				return applyReplacement(translated, {
+                    'count': options.count
+                });
+			}
+		}
+
+		var keys = dottedkey.split(o.keyseparator);
+		var i = 0;
+		var value = dictionary;
+        while (keys[i]) {
+            value = value && value[keys[i]];
+            i++;
+        }
+		if (value) {
+			value = applyReplacement(value, options);
+			value = applyReuse(value, options);
+			return value;
+		} else {
+			return notFound;
+		}
+	};
+
+	var translate = function(dottedkey, options) {
+		countRecursion = 0;
+		return doTranslate(dottedkey, options);
+	};
+
+    var translateCapital = function(dottedKey, options) {
+        var text = translate(dottedKey, options);
+        return text.charAt(0).toUpperCase() + text.substr(1);
+    };
+
+    var translateUpperCase  = function(dottedKey, options) {
+        var text = translate(dottedKey, options);
+        return text.toUpperCase();
+    };
+
+	var lang = function(newLang) {
+        if (newLang) {
+            loadDictionary(newLang, function() {
+            });
+        } else {
+		    return currentLang;
+        }
+	};
+
+	$.jsperanto = function(callback, options) {
+		$.extend(o, options);
+
+		if (!o.lang) {
+            o.lang = detectLanguage();
+        }
+
+		loadDictionary(o.lang, function(loadedLang) {
+			currentLang = loadedLang;
+
+            if (o.setDollarT) {
+                $.t = translate;
+                $.T = translateCapital;
+                $.U = translateUpperCase;
+            }
+
+			callback(translate);
+		});
+    };
+
+    $.extend($.jsperanto, {
+        'translate': translate,
+
+		't': translate,
+
+        'T': translateCapital,
+
+        'U': translateUpperCase,
+
+        'detectLanguage': detectLanguage,
+
+		'lang': lang
+	});
+})(this, this.document, this.jQuery);
--- test/test.js
+++ test/test.js
@@ -1,38 +1,48 @@
-asyncTest("translation", function() {
+asyncTest('translation', function() {
     var o = {
 		lang : 'testlang'
 	};
-	$.jsperanto.init(function(t){
-        equals(t('product.name'),"jsperanto");
-        equals(t('withreuse'),"jsperanto and Home");
-        equals(t('withreplacement',{year:2005}),"since 2005");
-        equals(t('4.level.of.nesting'),"4 level of nesting");
-        equals(t('not.existing.key',{defaultValue:"default"}),"default");
-        equals(t('product.name'),"jsperanto");
-        equals(t('pluralversionexist',{count:2}),"plural version of pluralversionexist");
-        equals(t('pluralversionexist',{count:1}),"singular version of pluralversionexist");
-        equals(t('pluralversionexist'),"singular version of pluralversionexist");
-        equals(t('pluralversiondoesnotexist',{count:2}),"plural version does not exist");
-        equals(t('withHTML'),"<b>this would be bold</b>");
+	$.jsperanto(function(t) {
+        equal(t('product.name'), 'jsperanto');
 
-        equals(t('count and replacement',{count:1}),"you have 1 friend");
-        equals(t('count and replacement',{count:3}),"you have 3 friends");
+        equal(t('withreuse'), 'jsperanto and Home');
 
-        
-		
-		equals($.t('can_speak_plural',{count:'any'}),"I can speak any languages","count can be a string");
-		
-		equals($.t('project.size.source',{value:4,unit:"kb"}),"jsperanto is 4 kb","Interpolation variables can be a number");
-		equals($.t('project.size.min',{value:1010,unit:"bytes"}),"jsperanto is 1010 bytes when minified","options are also used for nested lookup ");
-		equals($.t('project.size.gzip',{value:505,unit:"bytes"}),"jsperanto is 505 bytes when minified and gzipped","options are also used for nested lookup");
-		
-        equals(t('not.existing.key'),"not.existing.key");
-		
+        equal(t('withreplacement', {year: 2005}), 'since 2005');
+
+        equal(t('4.level.of.nesting'), '4 level of nesting');
+
+        equal(t('not.existing.key', {defaultValue: 'default'}), 'default');
+
+        equal(t('product.name'), 'jsperanto');
+
+        equal(t('pluralversionexist', {count: 2}), 'plural version of pluralversionexist');
+
+        equal(t('pluralversionexist', {count: 1}), 'singular version of pluralversionexist');
+
+        equal(t('pluralversionexist'), 'singular version of pluralversionexist');
+
+        notEqual(t('pluralversiondoesnotexist', {count: 2}), 'plural version does not exist');
+
+        equal(t('withHTML'), '<b>this would be bold</b>');
+
+        equal(t('count and replacement', {count: 1}), 'you have 1 friend');
+
+        equal(t('count and replacement', {count: 3}), 'you have 3 friends');
+
+		equal($.t('can_speak_plural', {count: 'any'}), 'I can speak any languages', 'count can be a string');
+
+		equal($.t('project.size.source', {value: 4, unit: 'kb'}), 'jsperanto is 4 kb', 'Interpolation variables can be a number');
+
+		equal($.t('project.size.min', {value: 1010, unit: 'bytes'}), 'jsperanto is 1010 bytes when minified', 'options are also used for nested lookup ');
+
+        equal($.t('project.size.gzip', {value: 505, unit: 'bytes'}), 'jsperanto is 505 bytes when minified and gzipped', 'options are also used for nested lookup');
+
+        equal(t('not.existing.key'), 'not.existing.key');
+
 		t('infinite');
-		equals(true,true,"recursive nested lookup should not crash");
-        
-		start();
-    },o);
-});
 
-//TODO test options
+        equal(true, true, 'recursive nested lookup should not crash');
+
+		start();
+    },  o);
+});
