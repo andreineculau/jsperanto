0ce086d9a4434d1cc44e2ea31ddab04bae99b79e
Andrei Neculau <andrei.neculau@klarna.com>
2011-09-22 10:39:39 +0200

    fixed indentation, and camelcase for keySeparator

--- jquery.jsperanto.js
+++ jquery.jsperanto.js
@@ -37,69 +37,69 @@
     var currentLocale = false;
 
     var detect = function() {
-	if (navigator) {
-	    return navigator.language ? navigator.language : navigator.userLanguage;
-	} else {
-	    return o.fallbackLocale;
-	}
+	    if (navigator) {
+	        return navigator.language ? navigator.language : navigator.userLanguage;
+	    } else {
+	        return o.fallbackLocale;
+	    }
     };
 
     var loadDictionary = function(locale, doneCallback) {
-	if (o.dictionary) {
-	    dictionary = o.dictionary;
-	    doneCallback(locale);
-	    return;
-	}
+	    if (o.dictionary) {
+	        dictionary = o.dictionary;
+	        doneCallback(locale);
+	        return;
+	    }
 
-	$.ajax({
-	    'url': o.dictionaryPath + '/' + locale + '.json',
+	    $.ajax({
+	        'url': o.dictionaryPath + '/' + locale + '.json',
 
-	    'success': function(data, status, xhr) {
-		dictionary = data;
-		doneCallback(locale);
-	    },
+	        'success': function(data, status, xhr) {
+		        dictionary = data;
+		        doneCallback(locale);
+	        },
 
-	    'error': function(xhr, status, error) {
-		if (locale != o.fallbackLocale) {
-		    loadDictionary(o.fallbackLocale, doneCallback);
-		} else {
-		    doneCallback(false);
-		}
-	    },
+	        'error': function(xhr, status, error) {
+		        if (locale != o.fallbackLocale) {
+		            loadDictionary(o.fallbackLocale, doneCallback);
+		        } else {
+		            doneCallback(false);
+		        }
+	        },
 
-	    'dataType': 'json'
-	});
+	        'dataType': 'json'
+	    });
     };
 
     var applyReplacement = function(string, replacementHash) {
-	$.each(replacementHash, function(key, value) {
-	    string = string.replace([o.interpolationPrefix, key, o.interpolationSuffix].join(''), value);
-	});
-	return string;
+	    $.each(replacementHash, function(key, value) {
+	        string = string.replace([o.interpolationPrefix, key, o.interpolationSuffix].join(''), value);
+	    });
+	    return string;
     };
 
     var applyReuse = function(translated, options) {
-	while (translated.indexOf(o.reusePrefix) != -1) {
-	    countRecursion++;
+	    while (translated.indexOf(o.reusePrefix) != -1) {
+	        countRecursion++;
 
             // safety net for too much recursion
-	    if (countRecursion > o.maxRecursion) {
+	        if (countRecursion > o.maxRecursion) {
                 break;
             }
 
-	    var indexOfOpening = translated.indexOf(o.reusePrefix);
-	    var indexOfEndOfClosing = translated.indexOf(o.reuseSuffix, indexOfOpening) + o.reuseSuffix.length;
-	    var token = translated.substring(indexOfOpening, indexOfEndOfClosing);
-	    var tokenSansSymbols = token.replace(o.reusePrefix, '').replace(o.reuseSuffix, '');
-	    var translatedToken = doTranslate(tokenSansSymbols, options);
+	        var indexOfOpening = translated.indexOf(o.reusePrefix);
+	        var indexOfEndOfClosing = translated.indexOf(o.reuseSuffix, indexOfOpening) + o.reuseSuffix.length;
+	        var token = translated.substring(indexOfOpening, indexOfEndOfClosing);
+	        var tokenSansSymbols = token.replace(o.reusePrefix, '').replace(o.reuseSuffix, '');
+	        var translatedToken = doTranslate(tokenSansSymbols, options);
 
-	    translated = translated.replace(token, translatedToken);
-	}
-	return translated;
+	        translated = translated.replace(token, translatedToken);
+	    }
+	    return translated;
     };
 
     var needsPlural = function(options) {
-	return (options.count && typeof options.count !== 'string' && options.count > 1);
+	    return (options.count && typeof options.count !== 'string' && options.count > 1);
     };
 
 
@@ -108,50 +108,50 @@
       options.count
     */
     var doTranslate = function(dottedkey, options) {
-	options = $.extend({}, options);
+	    options = $.extend({}, options);
 
-	var notFound = options.defaultValue ? options.defaultValue : dottedkey;
+	    var notFound = options.defaultValue ? options.defaultValue : dottedkey;
 
         // No dictionary to translate from
-	if (!dictionary) {
+	    if (!dictionary) {
             return notFound;
         }
 
-	if (needsPlural(options)) {
-	    var optionsPlural = $.extend({}, options);
+	    if (needsPlural(options)) {
+	        var optionsPlural = $.extend({}, options);
             delete optionsPlural.count;
             optionsPlural.defautValue = pluralNotFound;
 
-	    var pluralKey = dottedkey + o.pluralSuffix;
-	    var translated = translate(pluralKey, optionsPlural);
+	        var pluralKey = dottedkey + o.pluralSuffix;
+	        var translated = translate(pluralKey, optionsPlural);
 
             //apply replacement for count only
-	    if (translated !== pluralNotFound) {
-		return applyReplacement(translated, {
+	        if (translated !== pluralNotFound) {
+		        return applyReplacement(translated, {
                     'count': options.count
                 });
+	        }
 	    }
-	}
 
-	var keys = dottedkey.split(o.keyseparator);
-	var i = 0;
-	var value = dictionary;
+	    var keys = dottedkey.split(o.keySeparator);
+	    var i = 0;
+	    var value = dictionary;
         while (keys[i]) {
             value = value && value[keys[i]];
             i++;
         }
-	if (value) {
-	    value = applyReplacement(value, options);
-	    value = applyReuse(value, options);
-	    return value;
-	} else {
-	    return notFound;
-	}
+	    if (value) {
+	        value = applyReplacement(value, options);
+	        value = applyReuse(value, options);
+	        return value;
+	    } else {
+	        return notFound;
+	    }
     };
 
     var translate = function(dottedkey, options) {
-	countRecursion = 0;
-	return doTranslate(dottedkey, options);
+	    countRecursion = 0;
+	    return doTranslate(dottedkey, options);
     };
 
     var translateCapital = function(dottedKey, options) {
@@ -165,42 +165,42 @@
     };
 
     var locale = function() {
-	return currentLocale;
+	    return currentLocale;
     };
 
     $.jsperanto = function(callback, options) {
-	$.extend(o, options);
+	    $.extend(o, options);
 
-	if (!o.locale || o.locale === 'detect') {
+	    if (!o.locale || o.locale === 'detect') {
             o.locale = detect();
         }
 
-	loadDictionary(o.locale, function(loadedLocale) {
-	    currentLocale = loadedLocale;
+	    loadDictionary(o.locale, function(loadedLocale) {
+	        currentLocale = loadedLocale;
 
             if (o.setDollarT) {
                 $.t = translate;
                 $.T = translateCapital;
                 $.U = translateUpperCase;
             }
-
+            
             if (typeof callback === 'function') {
-		callback(translate);
+		        callback(translate);
             }
-	});
+	    });
     };
-
+    
     $.extend($.jsperanto, {
         'translate': translate,
-
-	't': translate,
+        
+	    't': translate,
 
         'T': translateCapital,
-
+        
         'U': translateUpperCase,
 
         'detect': detect,
 
-	'locale': locale
+	    'locale': locale
     });
 })(this, this.document, this.jQuery);
